39418d2cb0e119620600110b47ef1a008868d9c1
Eelis-Kokkonen <kokkonee@gmail.com>
2026-08-19 10:26:18 +0300

    Update model.py

--- models/model.py
+++ models/model.py
@@ -1,1 +1,47 @@
+import torch
+import torch.nn
+import torch.nn.Functional as F
+
+
+class Model(nn.Module):
+    def __init__(self, 
+                 input_dim=16, 
+                 d_model=64, 
+                 d_ff=128, 
+                 num_layers=4, 
+                 output_dim=12):
+
+        self.latent = nn.Parameter(torch.randn(1, 1, d_model))
+
+        self.input = nn.Linear(input_dim, d_model)
+
+        self.attention = Transformer(d_model=d_model, 
+                                     n_head=2, 
+                                     num_encoder_layers=0, 
+                                     num_decoder_layers=num_layers, 
+                                     dim_feedforward=d_ff, 
+                                     activation=F.SiLU, 
+                                     batch_first=True)
+
+        self.output = nn.Linear(d_model, output_dim)  
+
+    def forward(self, x):
+
+        B = x.shape(0)
+      
+        x = self.input(x)
+
+        latent = self.latent.expand(B, -1, -1)
+
+        x = torch.cat(latent, x)
+      
+        x = self.attention(x)
+
+        x = x[:, 0, :]
+      
+        x = self.output(x)
+        
+        return x
+
+
 
