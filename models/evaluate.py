0e81b8ab416d8969e58411398d59f200b702bb0e
Eelis-Kokkonen <kokkonee@gmail.com>
2026-08-20 14:56:50 +0300

    Update evaluate.py

--- models/evaluate.py
+++ models/evaluate.py
@@ -1,1 +1,23 @@
+from model import Model
+from physics.simulator import sim
+
+import torch
+
+
+
+
+
+class Evaluation:
+    def __init__(self, simulation=sim, model=Model):
+
+
+        self.simulation = simulation
+        self.model = Model
+
+
+    def evaluate(self, steps=10_000):
+
+
+        for step in steps:
+            model.train()
 
