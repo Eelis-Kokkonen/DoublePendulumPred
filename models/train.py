fe1115d61f5407c8804beb29ca814f6ddb084f7b
Eelis-Kokkonen <kokkonee@gmail.com>
2026-09-02 15:28:12 +0300

    Update train.py

--- models/train.py
+++ models/train.py
@@ -1,5 +1,5 @@
 from model import Model
-from physics.simulator import sim
+from data.generate_data import generate_block
 
 import torch
 
@@ -8,16 +8,70 @@ import torch
 
 
 class Training:
-    def __init__(self, simulation=sim, model=Model):
+    def __init__(self, 
+                 optimizer, 
+                 loss_fn, 
+                 shcedular,
+                 device,
+                 data_gen=generate_block, 
+                 model=Model
+                ):
 
+        self.device = device
+        self.model = model
 
-        self.simulation = simulation
-        self.model = Model
+        self.loss_fn = loss_fn
+        self.optimizer = optimizer
+        self.schedular = schedular
+                    
+        self.data_gen = data_gen
 
+        self.eval_freq = 1_000
 
-    def train(self, steps=10_000):
-
+    def train(self, steps=1_000):
+        
+        print("Training has started...")
 
         for step in steps:
             model.train()
 
+            self.optimizer.zero_grad()
+
+            traj = data_gen()
+
+            train_traj
+            eval_traj
+
+            pred = model.predict(train_traj, timesteps=1_000)
+
+            loss = self.loss_fn(pred, eval_traj)
+
+            loss.backwards()
+
+            self.optimizer.step()
+
+            if schedular is not None:
+                self.schedular.step()
+
+            if (step + 1)  % eval_freq == 0:
+                model.eval()
+                
+                traj_eval = data_gen()
+
+                input_traj
+                pred_traj
+
+                pred_eval = model.predict(input_traj, timesteps=1_000)
+
+                loss = self.loss_fn(pred, eval_traj)
+
+                torch.save({
+                    "model_state_dict": model.state_dict(),
+                    "optimizer_state_dict": optimizer.state_dict(),
+                    "schedular_state_dict": schedular.state_dict() if schedular else None,
+                    "steps": steps,
+                    "loss": loss
+                }, "checkpoint.pth")
+
+        print("Training has ended...")
+        
