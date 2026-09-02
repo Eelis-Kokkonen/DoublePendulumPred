a596b80e5ceabb528c0acb6d277e62ca174eed75
Eelis-Kokkonen <kokkonee@gmail.com>
2026-09-02 14:36:06 +0300

    Update generate_data.py

--- data/generate_data.py
+++ data/generate_data.py
@@ -1,9 +1,33 @@
-from .physics import simulation
+import numba
 
-class dataset:
-    def __init__(self):
+
+@nb.jit
+def generate_block(
+    initial_states,
+    dt,
+    timesteps,
+    l1,
+    l2,
+    m1,
+    m2,
+    g
+):
+    num_sims = initial_states.shape[0]
+
+    trajectories = np.zeros(num_sims, timesteps, 0)
+
+    for i in nb.prange(num_sims):
         
-        
-        
-    def generate(self):
-        
+        trajectories[i] = gen_traj(
+            initial_states[i],
+            dt,
+            timesteps,
+            l1,
+            l2,
+            m1,
+            m2,
+            g
+        )
+
+    return trajectories
+
