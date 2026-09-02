754ab123e92d47b72d913fd77fa69ca5aeac986e
Eelis-Kokkonen <kokkonee@gmail.com>
2026-09-02 14:59:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,35 @@ from models.model import Model
 from models.training import Training
 from data.generate_data import generate_block
 
+import torch.nn
+import torch
 
 if __name__ == "__main__":
 
-    trainer = Training(data_gen=generate_block, model=Model)
+    optimizer = nn.AdamW()
 
+    loss_fn = 
+    
+    schedular = 
+
+
+    trainer = Trainer(
+        optimizer=optimizer, 
+        loss_fn=loss_fn, 
+        schedular=schedular,
+        data_gen=generate_block, 
+        model=Model
+    )
+    
+    
+    
     trainer.train(steps=1_000)
+
+
+                 optimizer, 
+                 loss_fn, 
+                 shcedular,
+                 device,
+                 data_gen=generate_block, 
+                 model=Model
+
