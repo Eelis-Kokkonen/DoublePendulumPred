cdf37fd9ed8ef6fd4476dae0eb55a7708e38aeb2
Eelis-Kokkonen <kokkonee@gmail.com>
2026-09-02 15:19:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,30 +7,41 @@ import torch
 
 if __name__ == "__main__":
 
-    optimizer = nn.AdamW()
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available:
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
 
-    loss_fn = 
+    steps = 1_000
+
+    model = Model(device=device)
     
-    schedular = 
+    optimizer = torch.optim.AdamW(
+        params=model.parameters(),
+        lr=1e-4,
+        betas=(0.9, 0.999),
+        eps=1e-8,
+        weight_decay=0.01
+    )
+    
+    schedular = torch.optim.CosineAnnealingLR(
+        optimizer=optimizer,
+        t_max=steps,
+        eta_min=0.0,
+        last_epoch=-1
+    )
 
+    loss_fn = torch.nn.MSELoss()
 
     trainer = Trainer(
         optimizer=optimizer, 
         loss_fn=loss_fn, 
         schedular=schedular,
+        device=device
         data_gen=generate_block, 
-        model=Model
+        model=model
     )
     
-    
-    
-    trainer.train(steps=1_000)
-
-
-                 optimizer, 
-                 loss_fn, 
-                 shcedular,
-                 device,
-                 data_gen=generate_block, 
-                 model=Model
-
+    trainer.train(steps=steps)
