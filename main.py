6432c0ddb9ea2061abfaf16430987ad6398e3397
Eelis-Kokkonen <kokkonee@gmail.com>
2026-09-02 14:46:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from models.model import Model
+from models.training import Training
+from data.generate_data import generate_block
 
+
+if __name__ == "__main__":
+
+    trainer = Training(data_gen=generate_block, model=Model)
+
+    trainer.train(steps=1_000)
