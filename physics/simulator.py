1ef52be812cdcb6f2776b6040b4a2491fd1037e2
Eelis-Kokkonen <kokkonee@gmail.com>
2026-09-05 17:37:51 +0300

    Update simulator.py

--- physics/simulator.py
+++ physics/simulator.py
@@ -69,10 +69,12 @@ def derivatives(state, l1, l2, m1, m2, g):
     # Opening Given State
     theta1, omega1, theta2, omega2 = state
 
+    eps = 1e-8
+
     
     delta = theta2 - theta1
-    den1 = (m1 + m2) * l1 - m2 * l1 * np.cos(delta) ** 2
-    den2 = (l2 / l1) * den1
+    den1 = (m1 + m2) * l1 - m2 * l1 * np.cos(delta) ** 2 + eps
+    den2 = (l2 / l1) * den1 + eps
 
     # Calculating Angular Velocity for First Pendulum
     domega1 = (m2 * l1 * omega1**2 * np.sin(delta) * np.cos(delta) + 
@@ -105,8 +107,17 @@ def gen_traj(state0, dt, timesteps, l1, l2, m1, m2, g):
         k4 = derivatives(s + dt * k3, l1, l2, m1, m2, g)
 
         # Calculating final Approximation
-        states[i + 1] = s + (dt / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4)
-        
+        next_s = s + (dt / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4)
+
+        next_s[0] = (next_s[0] + np.pi) % (2 * np.pi) - np.pi
+        next_s[2] = (next_s[2] + np.pi) % (2 * np.pi) - np.pi
+
+        next_s[1] = np.clip(next_s[1], -100.0, 100.0)
+        next_s[4] = np.clip(next_s[3], -100.0, 100.0)
+
+        states[i + 1] = next_s
+
+    
     return states
         
 
