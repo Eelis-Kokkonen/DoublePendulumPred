4f8f10f46744d98a104c7dda182e5127056051e2
Eelis-Kokkonen <kokkonee@gmail.com>
2026-09-05 16:21:04 +0300

    Update train.py

--- models/train.py
+++ models/train.py
@@ -62,6 +62,8 @@ class Training:
 
             loss.backward()
 
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
+
             self.optimizer.step()
 
             pbar.set_postfix(
