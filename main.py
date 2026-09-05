e6d22cb97af4ae84da6129ef084e5ab548c9eb8e
Eelis-Kokkonen <kokkonee@gmail.com>
2026-09-05 16:36:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     if torch.cuda.is_available():
         device = torch.device("cuda")
-    elif torch.backends.mps.is_available:
+    elif torch.backends.mps.is_available():
         device = torch.device("mps")
     else:
         device = torch.device("cpu")
